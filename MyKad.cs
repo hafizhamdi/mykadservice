c4584675237d3d54b21e2419b74f9d2070525d19
hafizhamdi <30652012+hafizhamdi@users.noreply.github.com>
2017-11-29 15:11:08 +0800

    Add files via upload
    Initial version

---
+++ MyKad.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCSC;
+using System.IO;
+
+namespace ServiceConsole
+{
+    class MyKad
+    {
+        private static SCardContext m_hcontext;
+        private static SCardReader m_reader;
+        private static SCardError m_err;
+        private static IntPtr m_pioSendPci;
+
+        public static string name;
+        public static string origname;
+        public static string gpcname;
+        public static string icno;
+        public static string dob;
+        public static string pob;
+        public static string gender;
+        public static string citizenship;
+        public static string religion;
+        public static string race;
+        public static string addr1;
+        public static string addr2;
+        public static string addr3;
+        public static string poscode;
+        public static string city;
+        public static string state;
+        public static string photo;
+
+        public static void Init()
+        {
+            // Establish Scard Context
+            m_hcontext = new SCardContext();
+            m_hcontext.Establish(SCardScope.System);
+
+            // Retrieve the list of Smartcard readers
+            string[] szReaders = m_hcontext.GetReaders();
+            if (szReaders.Length <= 0)
+                throw new PCSCException(SCardError.NoReadersAvailable,
+                    "Could not found any Smartcard readers.");
+
+            Console.WriteLine("reader name: " + szReaders[0]);
+
+            // Create a reader object using the existing context
+            m_reader = new SCardReader(m_hcontext);
+
+            // Connect to the card
+            m_err = m_reader.Connect(szReaders[0],
+                SCardShareMode.Shared,
+                SCardProtocol.T0 | SCardProtocol.T1);
+            CheckErr(m_err);
+
+            switch (m_reader.ActiveProtocol)
+            {
+                case SCardProtocol.T0:
+                    m_pioSendPci = SCardPCI.T0;
+                    break;
+                case SCardProtocol.T1:
+                    m_pioSendPci = SCardPCI.T1;
+                    break;
+                default:
+                    throw new PCSCException(SCardError.ProtocolMismatch,
+                        "Protocol not supported: "
+                        + m_reader.ActiveProtocol.ToString());
+            }
+        }
+
+        public static void SelectJPN()
+        {
+            string s1, s2;
+            s1 = APCOM.SELECT_JPN_APPLICATION.Replace(" ", "");
+            s2 = APCOM.SELECT_APPLICATION_GET_RESPONSE.Replace(" ", "");
+
+            //Preparing apdu cmd
+            byte[][] send = { hexToByteArr(s1), hexToByteArr(s2) };
+            try
+            {
+                byte[] response = new byte[256];
+                for (int t = 0; t < send.Length; t++)
+                {
+                    response = new byte[256];
+
+                    // Send SELECT command
+                    m_err = m_reader.Transmit(m_pioSendPci, send[t], ref response);
+                    //Console.Write("send: ");
+                    //for (int k = 0; k < send[t].Length; k++)
+                    //  Console.Write("{0:X2} ", send[t][k]);
+                    //Console.WriteLine();
+                    
+                    CheckErr(m_err);
+
+                    //Console.Write("response: ");
+                    //for (int i = 0; i < response.Length; i++)
+                    //    Console.Write("{0:X2} ", response[i]);
+                    //Console.WriteLine();
+
+                    Array.Clear(response, 0, 0);
+                }
+            }
+            catch (PCSCException ex)
+            {
+                Console.WriteLine("Ouch: "
+                    + ex.Message
+                    + " (" + ex.SCardError.ToString() + ")");
+            }
+        }
+
+        public static string GetFld(string[] fld, string pg)
+        {
+            //[SETLENGTH(8)][len(2)]
+            //[SELECTINFO(5)][JPNPAGE(4)][offset(2)][len(2)]
+            //[READINFO(4)][len(1)]
+
+            string s1, s2, s3, rs="";
+            s1 = APCOM.SET_LENGTH + fld[0] + "00";
+            s1 = s1.Replace(" ", "");
+
+            s2 = APCOM.SELECT_INFO + pg + fld[1] + fld[0] + "00";
+            s2 = s2.Replace(" ", "");
+
+            s3 = APCOM.READ_INFO + fld[0];
+            s3 = s3.Replace(" ", "");
+
+            //Preparing apdu cmd
+            byte[][] send = { hexToByteArr(s1), hexToByteArr(s2), hexToByteArr(s3) };
+            try
+            {
+                byte[] response = new byte[256];
+                for (int t = 0; t < send.Length; t++)
+                {
+                    response = new byte[256];
+
+                    // Send SELECT command
+                    m_err = m_reader.Transmit(m_pioSendPci, send[t], ref response);
+                    //Console.Write("send: ");
+                    //for (int k = 0; k < send[t].Length; k++)
+                    //    Console.Write("{0:X2} ", send[t][k]);
+                    //Console.WriteLine();
+
+                    CheckErr(m_err);
+
+                    //Console.Write("response: ");
+                    //for (int i = 0; i < response.Length; i++)
+                    //    Console.Write("{0:X2} ", response[i]);
+                    //Console.WriteLine();
+
+                    Array.Clear(response, 0, 0);
+                }
+                string check = BitConverter.ToString(response).Replace("-","");
+                if (!check.Contains("9000"))
+                {
+                    Console.WriteLine("Opps.. Getting fld failed.");
+                    return "";
+                }
+
+                string test = ba2s(response);
+                test = test.Replace("-", "");
+
+                byte[] res = new byte[response.Length - 2];
+                Array.Copy(response, res, response.Length - 2);
+                rs = Encoding.UTF8.GetString(res);
+            }
+            catch (PCSCException ex)
+            {
+                Console.WriteLine("Ouch: "
+                    + ex.Message
+                    + " (" + ex.SCardError.ToString() + ")");
+            }
+
+            return rs;
+        }
+
+        public static string GetFldNot(string[] fld, string pg, int need)
+        {
+            //[SETLENGTH(8)][len(2)]
+            //[SELECTINFO(5)][JPNPAGE(4)][offset(2)][len(2)]
+            //[READINFO(4)][len(1)]
+
+            string s1, s2, s3, rs = "";
+            s1 = APCOM.SET_LENGTH + fld[0] + "00";
+            s1 = s1.Replace(" ", "");
+
+            s2 = APCOM.SELECT_INFO + pg + fld[1] + fld[0] + "00";
+            s2 = s2.Replace(" ", "");
+
+            s3 = APCOM.READ_INFO + fld[0];
+            s3 = s3.Replace(" ", "");
+
+            //Preparing apdu cmd
+            byte[][] send = { hexToByteArr(s1), hexToByteArr(s2), hexToByteArr(s3) };
+            try
+            {
+                byte[] response = new byte[256];
+                for (int t = 0; t < send.Length; t++)
+                {
+                    response = new byte[256];
+
+                    // Send SELECT command
+                    m_err = m_reader.Transmit(m_pioSendPci, send[t], ref response);
+                    //Console.Write("send: ");
+                    //for (int k = 0; k < send[t].Length; k++)
+                    //    Console.Write("{0:X2} ", send[t][k]);
+                    //Console.WriteLine();
+
+                    CheckErr(m_err);
+                    
+                    if (t == 2)
+                    {
+                        //Console.Write("response: ");
+                        for (int i = 0; i < response.Length; i++)
+                        {
+                            //Console.Write("{0:X2} ", pbRecvBuffer[i]);
+                            rs = rs + string.Format("{0:X2}", response[i]);
+                        }
+                    }
+
+                    Array.Clear(response, 0, 0);
+                }
+                string check = BitConverter.ToString(response).Replace("-", "");
+                if (!check.Contains("9000"))
+                {
+                    Console.WriteLine("Opps.. Getting fld failed.");
+                    return "";
+                }
+
+                rs = rs.Substring(0, need);
+            }
+            catch (PCSCException ex)
+            {
+                Console.WriteLine("Ouch: "
+                    + ex.Message
+                    + " (" + ex.SCardError.ToString() + ")");
+            }
+
+            return rs;
+        }
+        
+        public static string GetPhoto()
+        {
+            //[SETLENGTH(8)][len(2)]
+            //[SELECTINFO(5)][JPNPAGE(4)][offset(2)][len(2)]
+            //[READINFO(4)][len(1)]
+
+            int len = 0xff, offset = 0x03;
+            int max = 4000;
+
+            string s1, s2, s3, res ="", sLen, sOff;
+            bool go = true;
+            string lastStr = "";
+            
+            do
+            {
+                if ((offset + len) > max)
+                    len = (max - offset);
+                else
+                    len = 0xff;
+                
+                sLen = len.ToString("X"); //FF
+                //Console.WriteLine("Remaining Length:" + sLen);
+                string tmp = offset.ToString("X2");
+                //Console.WriteLine("Tmp Offset:" + tmp);
+                if(tmp.Length <= 2)
+                    sOff = tmp + "00"; //0300
+                else
+                    sOff = tmp.Substring(1,2) +" 0" + tmp.Substring(0,1); //3000
+
+                //Console.WriteLine("Current Offset: " + sOff);               
+                s1 = APCOM.SET_LENGTH + sLen+ "00"; 
+                s1 = s1.Replace(" ", "");
+                //Console.WriteLine(s1);
+
+                s2 = APCOM.SELECT_INFO + APCOM.JPN[1] + sOff + sLen+"00";
+                s2 = s2.Replace(" ", "");
+                //Console.WriteLine(s2);
+
+                s3 = APCOM.READ_INFO + sLen;
+                s3 = s3.Replace(" ", "");
+                //Console.WriteLine(s3);
+
+                //Preparing apdu cmd
+                byte[][] send = { hexToByteArr(s1), hexToByteArr(s2), hexToByteArr(s3) };
+
+                try
+                {
+                    byte[] response = new byte[256];
+                    for (int t = 0; t < send.Length; t++)
+                    {
+                        if(t==2)
+                            response = new byte[300];
+                        else
+                            response = new byte[256];
+
+                        // Send SELECT command
+                        m_err = m_reader.Transmit(m_pioSendPci, send[t], ref response);
+
+                        //Console.Write("send: ");
+                        //for (int k = 0; k < send[t].Length; k++)
+                        //    Console.Write("{0:X2} ", send[t][k]);
+                        //Console.WriteLine();
+
+                        CheckErr(m_err);
+
+                        //Console.Write("response: ");
+                        //for (int i = 0; i < response.Length; i++)
+                        //    Console.Write("{0:X2} ", response[i]);
+                        //Console.WriteLine();
+
+                        if (t == 2)
+                        {/*
+                            Console.Write("send: ");
+                            for (int k = 0; k < send[t].Length; k++)
+                                Console.Write("{0:X2} ", send[t][k]);
+                            Console.WriteLine();
+                            */
+                            CheckErr(m_err);
+/*
+                            Console.Write("response: ");
+                            for (int i = 0; i < response.Length; i++)
+                                Console.Write("{0:X2} ", response[i]);
+                            Console.WriteLine();
+                            */
+                            len = response.Length-2; //remove 9000
+
+                            res = ba2s(response).Replace("-", "");
+                            
+                            //Console.WriteLine(res);
+                            if (res.Contains("FFD9")) //last bytes for photo
+                                go = false;
+
+                            res = res.Replace("9000", "");
+                            lastStr += res;
+                        }
+                        
+                        Array.Clear(response, 0, 0);
+                    }
+                    string check = BitConverter.ToString(response).Replace("-", "");
+                    if (!check.Contains("9000"))
+                    {
+                        Console.WriteLine("Opps.. Getting fld failed.");
+                        return null;
+                    }
+                }
+                catch (PCSCException ex)
+                {
+                    Console.WriteLine("Ouch: "
+                        + ex.Message
+                        + " (" + ex.SCardError.ToString() + ")");
+                }
+                
+                if ((offset + len) > max)
+                    offset += (max - offset);
+                else
+                    offset += len;
+            }
+            while (go);
+
+            lastStr = lastStr.Trim('0');
+    
+            byte[] imgBuf;
+            imgBuf = hexToByteArr(lastStr + "9"); //append missing niner
+
+            string base64 = Convert.ToBase64String(imgBuf);
+            
+            photo = base64;
+            return photo;
+        }
+
+        public static byte[] hexToByteArr(string hex)
+        {
+            if (hex.Length % 2 != 0)
+                return null;
+
+            byte[] bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
+
+        public static string ba2s(byte[] data)
+        {
+            return BitConverter.ToString(data);
+        }
+
+        public static void Flush()
+        {
+            m_hcontext.Cancel();
+
+            if (m_reader != null)
+            {
+                m_reader.Disconnect(SCardReaderDisposition.Unpower);
+                Console.WriteLine("Card Dispose.");
+            }
+        }
+
+        public static void ReadInfo()
+        {
+            try
+            {
+                if (m_hcontext != null)
+                {
+                    Console.WriteLine("Retrieving info started ...");
+                    string JPN1 = APCOM.JPN[0];
+                    string JPN4 = APCOM.JPN[3];
+
+                    //Page JPN-1-1
+                    name = GetFld(APCOM.NAME, JPN1);
+                    origname = GetFld(APCOM.ORIG_NAME, JPN1);
+                    gpcname = GetFld(APCOM.GPC_NAME, JPN1);
+                    icno = GetFld(APCOM.IC_NUMBER, JPN1);
+                    dob = GetFldNot(APCOM.DOB, JPN1, 8);
+                    gender = GetFld(APCOM.GENDER, JPN1);
+                    pob = GetFld(APCOM.BIRTH_PLACE, JPN1);
+                    race = GetFld(APCOM.RACE, JPN1);
+                    citizenship = GetFld(APCOM.CITIZENSHIP, JPN1);
+                    religion = GetFld(APCOM.RELIGION, JPN1);
+
+                    addr1 = GetFld(APCOM.ADDRESS_1, JPN4);
+                    addr2 = GetFld(APCOM.ADDRESS_2, JPN4);
+                    addr3 = GetFld(APCOM.ADDRESS_3, JPN4);
+                    poscode = GetFldNot(APCOM.POSCODE, JPN4, 5);
+                    city = GetFld(APCOM.CITY, JPN4);
+                    state = GetFld(APCOM.STATE, JPN4);
+                    photo = GetPhoto();
+                }
+            }
+            catch (PCSCException ex)
+            {
+                Console.WriteLine("Ouch: "
+                    + ex.Message
+                    + " (" + ex.SCardError.ToString() + ")");
+            }
+        }
+
+        public static void CheckErr(SCardError err)
+        {
+            if (err != SCardError.Success)
+                throw new PCSCException(err,
+                    SCardHelper.StringifyError(err));
+            
+        }
+        
+        
+    }
+
+    class APCOM
+    {
+        public static string[] NAME = { "28", "E9 00" };
+        public static string[] ORIG_NAME = { "96" , "03 00" };
+        public static string[] GPC_NAME = { "50", "99 00" };
+        public static string[] IC_NUMBER = { "0D", "11 01" };
+        public static string[] GENDER = { "01", "1E 01" };
+        public static string[] OLD_IC = { "08", "1F 01" };
+        public static string[] DOB = { "04", "27 01" };
+        public static string[] BIRTH_PLACE = { "19", "2B 01" };
+        public static string[] CITIZENSHIP = { "12", "48 01" };
+        public static string[] RACE = { "19", "5A 01" };
+        public static string[] RELIGION = { "0B", "73 01" };
+
+        public static string[] ADDRESS_1 = { "1E", "03 00" };
+        public static string[] ADDRESS_2 = { "1E", "21 00" };
+        public static string[] ADDRESS_3 = { "1E", "3F 00" };
+        public static string[] POSCODE = { "03", "5D 00" };
+        public static string[] CITY = { "19", "60 00" };
+        public static string[] STATE = { "1E", "79 00" };
+
+        public static string[] THUMBPRINT_RIGHT = { "FF", "17 00" };
+        public static string[] THUMBPRINT_LEFT = { "FF", "6D 02" };
+
+        public static string[] PHOTO = { "FF", "03 00"};
+
+        public static string ATR = "3B 67 00 00 73 20 00 6C 68 90 00";
+        public static string SELECT_JPN_APPLICATION = "00 A4 04 00 0A A0 00 00 00 74 4A 50 4E 00 10";
+        public static string SELECT_APPLICATION_GET_RESPONSE = "00 C0 00 00 05";
+
+        public static string SET_LENGTH = "C8 32 00 00 05 08 00 00";
+        public static string SELECT_INFO = "CC 00 00 00 08";
+        public static string READ_INFO = "CC 06 00 00";
+
+        public static string[] JPN = { "01 00 01 00", "02 00 01 00", "03 00 01 00",
+                                      "04 00 01 00", "05 00 01 00", "06 00 01 00"};
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using Newtonsoft.Json;
+
+using System.ComponentModel;
+using System.ServiceProcess;
+using System.Configuration;
+using System.Configuration.Install;
+
+namespace ServiceConsole
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json), CorsEnabled]
+        string Hello();
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json,
+           RequestFormat = WebMessageFormat.Json), CorsEnabled]
+        string readmykad();
+    }
+
+    public class Service : IService
+    {
+        public string Hello()
+        {
+            return "Hello Im In!";
+        }
+
+        public string readmykad()
+        {
+            MyKad.Init();
+            MyKad.SelectJPN();
+            MyKad.ReadInfo();
+            MyKad.Flush();
+
+            Person person = new Person();
+
+            person.name = MyKad.name.Trim(' ');
+            person.origname = MyKad.origname.Trim(' ');
+            person.gpcname = MyKad.gpcname.Trim(' ');
+            person.icno = MyKad.icno.Trim(' ');
+            person.dob = MyKad.dob.Trim(' ');
+            person.pob = MyKad.pob.Trim(' ');
+            person.gender = MyKad.gender.Trim(' ');
+            person.religion = MyKad.religion.Trim(' ');
+            person.race = MyKad.race.Trim(' ');
+            person.citizenship = MyKad.citizenship.Trim(' ');
+            person.addr1 = MyKad.addr1.Trim(' ');
+            person.addr2 = MyKad.addr2.Trim(' ');
+            person.addr3 = MyKad.addr3.Trim(' ');
+            person.city = MyKad.city.Trim(' ');
+            person.poscode = MyKad.poscode.Trim(' ');
+            person.state = MyKad.state.Trim(' ');
+            person.photo = MyKad.photo;
+
+            string sPerson = JsonConvert.SerializeObject(person);
+
+            return sPerson;
+        }
+
+        public class WindowsService : ServiceBase
+        {
+            public ServiceHost serviceHost = null;
+            public WindowsService()
+            {
+                // Name the Windows Service
+                ServiceName = "WCFWindowsServiceSample";
+            }
+
+            public static void Main()
+            {
+                ServiceBase.Run(new WindowsService());
+            }
+
+            // Start the Windows service.
+            protected override void OnStart(string[] args)
+            {
+                if (serviceHost != null)
+                {
+                    serviceHost.Close();
+                }
+                
+                serviceHost = new CorsEnabledServiceHost(typeof(Service), new Uri("http://localhost:8000/"));
+                try
+                {
+                    serviceHost.Open();
+                    using (ChannelFactory<IService> cf = new ChannelFactory<IService>(new WebHttpBinding(), "http://localhost:8000/"))
+                    {
+                        cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
+
+                        IService channel = cf.CreateChannel();
+
+
+                        string s;
+
+                        Console.WriteLine("Calling Hello via HTTP GET: ");
+                        s = channel.Hello();
+                        Console.WriteLine("   Output: {0}", s);
+
+                        Console.WriteLine("");
+                        Console.WriteLine("This can also be accomplished by navigating to");
+                        Console.WriteLine("http://localhost:8000/Hello");
+                        Console.WriteLine("in a web browser while this service is running.");
+                        //Console.WriteLine("http://localhost:8000/EchoWithGet?s=Hello, world!");
+                        Console.WriteLine("");
+                    }
+                    
+                }
+                catch (CommunicationException cex)
+                {
+                    Console.WriteLine("An exception occurred: {0}", cex.Message);
+                    serviceHost.Abort();
+                }
+                
+            }
+
+            protected override void OnStop()
+            {
+                if (serviceHost != null)
+                {
+                    serviceHost.Close();
+                    serviceHost = null;
+                }
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string name;
+        public string origname;
+        public string gpcname;
+        public string icno;
+        public string dob;
+        public string pob;
+        public string gender;
+        public string citizenship;
+        public string religion;
+        public string race;
+        public string addr1;
+        public string addr2;
+        public string addr3;
+        public string poscode;
+        public string city;
+        public string state;
+        public string photo;
+    }
+
+    class Program
+    {
+    }
+
+    // Provide the ProjectInstaller class which allows 
+    // the service to be installed by the Installutil.exe tool
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.StartType = ServiceStartMode.Automatic;
+            service.ServiceName = "MykadWinService";
+            service.DisplayName = "MyKad Windows Service";
+            service.Description = "Mykad service use to trigger smartcard reader";
+
+            Installers.Add(process);
+            Installers.Add(service);
+
+            //service.AfterInstall += Service_AfterInstall;
+        }
+
+        /*private void Service_AfterInstall(object sender, InstallEventArgs e)
+        {
+            ServiceController sc = new ServiceController("MyKad Windows Service");
+            try
+            {
+                //sc.Start(); old
+                sc.Start()
+
+            }
+            finally
+            {
+                sc.Close();
+            }
+        }*/
+    }
+}
